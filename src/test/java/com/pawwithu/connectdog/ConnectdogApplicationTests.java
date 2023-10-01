6a1e9c8e8de17334b7d5b7b428525737db144b0b
kyeong-hyeok <minkh4227@naver.com>
2023-10-01 12:33:58 +0900

    프로젝트 생성 (#1)

---
+++ src/test/java/com/pawwithu/connectdog/ConnectdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawwithu.connectdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectdogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
