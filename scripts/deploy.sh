ad864688c9a475cf91e25efcee9c7a31c93a8bb1
kyeong-hyeok <minkh4227@naver.com>
2023-10-02 15:56:07 +0900

    chore: deploy.sh 파일 수정 (#3)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,8 +29,6 @@ if [ -z "$EXIST_BLUE" ]; then
 
   # blue가 현재 실행 중이지 않다면 -> 런타임 에러 또는 다른 이유로 배포가 되지 못한 상태
   if [ -z "$BLUE_HEALTH" ]; then
-    # slack으로 알람을 보낼 수 있는 스크립트를 실행
-    sudo ./slack_blue.sh
   # blue가 현재 실행되고 있는 경우에만 green을 종료
   else
 
@@ -57,7 +55,6 @@ else
 
   if [ -z "$GREEN_HEALTH" ]; then
 
-      sudo ./slack_green.sh
   else
 
       # /home/ubuntu/deploy.log: 로그 파일에 "blue 중단 시작"이라는 내용을 추가
