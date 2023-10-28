df87af1250723979c4595b8ca8f81971e1a4a8d3
kyeong-hyeok <minkh4227@naver.com>
2023-10-28 14:13:03 +0900

    feat: Valid 에러 처리 Handler 추가 (#16)

--- src/main/java/com/pawwithu/connectdog/error/exception/GlobalControllerAdvice.java
+++ src/main/java/com/pawwithu/connectdog/error/exception/GlobalControllerAdvice.java
@@ -6,8 +6,15 @@ import com.pawwithu.connectdog.error.exception.custom.TokenException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Slf4j
 @RestControllerAdvice
@@ -29,6 +36,18 @@ public class GlobalControllerAdvice {
     public ResponseEntity<ErrorResponse> handle(final TokenException e) {
         log.info("Invalid Token: {}", e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.of(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        // 에러 반환이 여러 개일 경우 첫 번째 에러를 반환
+        String firstErrorMessage = bindingResult.getFieldErrors().get(0).getDefaultMessage();
+        // 모든 에러 메시지는 로그로 남김
+        List<String> errorList = bindingResult.getFieldErrors().stream().map(err -> err.getDefaultMessage()).collect(Collectors.toList());
+        log.warn("MethodArgumentNotValidExceptionException = {}", errorList);
+
+        return ResponseEntity.status(BAD_REQUEST).body(ErrorResponse.of("V1", firstErrorMessage));
     }
 
 
