876fbe3a0edaa665047991a00985d2747919054c
kyeong-hyeok <minkh4227@naver.com>
2023-10-28 14:56:04 +0900

    chore: OpenAPIDefinition 추가 (#16)

--- src/main/java/com/pawwithu/connectdog/ConnectdogApplication.java
+++ src/main/java/com/pawwithu/connectdog/ConnectdogApplication.java
@@ -1,5 +1,7 @@
 package com.pawwithu.connectdog;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @EnableCaching
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "https://dev-api.connectdog.site")})
 public class ConnectdogApplication {
 
 	public static void main(String[] args) {
