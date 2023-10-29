330127bf8e28edf72a625aa2731c93c8628437c7
kyeong-hyeok <minkh4227@naver.com>
2023-10-29 15:16:13 +0900

    feat: 이동봉사 중개 자체 회원가입 API 구현 (#21)

--- src/main/java/com/pawwithu/connectdog/domain/auth/controller/SignUpController.java
+++ src/main/java/com/pawwithu/connectdog/domain/auth/controller/SignUpController.java
@@ -1,6 +1,7 @@
 package com.pawwithu.connectdog.domain.auth.controller;
 
 import com.pawwithu.connectdog.domain.auth.dto.request.EmailRequest;
+import com.pawwithu.connectdog.domain.auth.dto.request.IntermediarySignUpRequest;
 import com.pawwithu.connectdog.domain.auth.dto.request.VolunteerSignUpRequest;
 import com.pawwithu.connectdog.domain.auth.dto.response.EmailResponse;
 import com.pawwithu.connectdog.domain.auth.service.AuthService;
@@ -13,10 +14,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "Sign-Up", description = "Sign-Up API")
 @RestController
@@ -29,13 +33,28 @@ public class SignUpController {
     @Operation(summary = "이동봉사자 자체 회원가입", description = "이동봉사자 자체 회원가입을 합니다.",
             responses = {@ApiResponse(responseCode = "204", description = "이동봉사자 자체 회원가입 성공")
                     , @ApiResponse(responseCode = "400"
-                    , description = "V1, 이메일 형식에 맞지 않습니다. \t\n V1, 이메일은 필수 입력 값입니다. \t\n V1, 영문+숫자 10자 이상 또는 영문+숫자+특수기호 8자 이상을 입력해 주세요." +
-                    "\t\n V1, 닉네임은 한글, 숫자만 사용가능합니다. \t\n V1, 닉네임은 필수 입력 값입니다. \t\n V1, 2~10자의 닉네임이어야 합니다 \t\n A1, 이미 존재하는 이메일입니다. \t\n A2, 이미 존재하는 사용자 닉네임입니다."
+                    , description = "V1, 이메일 형식에 맞지 않습니다. \t\n V1, 이메일은 필수 입력 값입니다. \t\n V1, 영문+숫자 10자 이상 또는 영문+숫자+특수기호 8자 이상을 입력해 주세요. \t\n " +
+                    "V1, 닉네임은 한글, 숫자만 사용가능합니다. \t\n V1, 닉네임은 필수 입력 값입니다. \t\n V1, 2~10자의 닉네임이어야 합니다 \t\n A1, 이미 존재하는 이메일입니다. \t\n A2, 이미 존재하는 사용자 닉네임입니다."
                     , content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
             })
     @PostMapping("/volunteers/sign-up")
-    public ResponseEntity<Void> volunteerSignUp(@RequestBody @Valid VolunteerSignUpRequest volunteerSignUpRequest) {
-        authService.volunteerSignUp(volunteerSignUpRequest);
+    public ResponseEntity<Void> volunteerSignUp(@RequestBody @Valid VolunteerSignUpRequest request) {
+        authService.volunteerSignUp(request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "이동봉사 중개 자체 회원가입", description = "이동봉사 중개 자체 회원가입을 합니다.",
+            responses = {@ApiResponse(responseCode = "204", description = "이동봉사 중개 자체 회원가입 성공")
+                    , @ApiResponse(responseCode = "400"
+                    , description = "V1, 이메일 형식에 맞지 않습니다. \t\n V1, 이메일은 필수 입력 값입니다. \t\n " +
+                    "V1, 영문+숫자 10자 이상 또는 영문+숫자+특수기호 8자 이상을 입력해 주세요. \t\n V1, url 형식을 입력해 주세요. \t\n " +
+                    "A1, 이미 존재하는 이메일입니다. \t\n F1, 파일이 존재하지 않습니다. \t\n F2, 파일 업로드에 실패했습니다."
+                    , content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            })
+    @PostMapping(value = "/intermediaries/sign-up", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Void> intermediarySignUp(@RequestPart @Valid IntermediarySignUpRequest request,
+                                                   @RequestPart(name = "file", required = false) MultipartFile file) {
+        authService.intermediarySignUp(request, file);
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/com/pawwithu/connectdog/domain/auth/service/AuthService.java
+++ src/main/java/com/pawwithu/connectdog/domain/auth/service/AuthService.java
@@ -1,6 +1,10 @@
 package com.pawwithu.connectdog.domain.auth.service;
 
+import com.pawwithu.connectdog.common.s3.FileService;
+import com.pawwithu.connectdog.domain.auth.dto.request.IntermediarySignUpRequest;
 import com.pawwithu.connectdog.domain.auth.dto.request.VolunteerSignUpRequest;
+import com.pawwithu.connectdog.domain.intermediary.entity.Intermediary;
+import com.pawwithu.connectdog.domain.intermediary.repository.IntermediaryRepository;
 import com.pawwithu.connectdog.domain.volunteer.entity.Volunteer;
 import com.pawwithu.connectdog.domain.volunteer.repository.VolunteerRepository;
 import com.pawwithu.connectdog.error.exception.custom.BadRequestException;
@@ -9,9 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
-import static com.pawwithu.connectdog.error.ErrorCode.ALREADY_EXIST_EMAIL;
-import static com.pawwithu.connectdog.error.ErrorCode.ALREADY_EXIST_NICKNAME;
+import static com.pawwithu.connectdog.error.ErrorCode.*;
 
 @Slf4j
 @Service
@@ -20,20 +24,34 @@ import static com.pawwithu.connectdog.error.ErrorCode.ALREADY_EXIST_NICKNAME;
 public class AuthService {
 
     private final VolunteerRepository volunteerRepository;
+    private final IntermediaryRepository intermediaryRepository;
     private final PasswordEncoder passwordEncoder;
+    private final FileService fileService;
 
-    public void volunteerSignUp(VolunteerSignUpRequest volunteerSignUpRequest) {
+    public void volunteerSignUp(VolunteerSignUpRequest request) {
 
-        if (volunteerRepository.existsByEmail(volunteerSignUpRequest.email())) {
+        if (volunteerRepository.existsByEmail(request.email())) {
             throw new BadRequestException(ALREADY_EXIST_EMAIL);
         }
-        if (volunteerRepository.existsByNickname(volunteerSignUpRequest.nickname())) {
+        if (volunteerRepository.existsByNickname(request.nickname())) {
             throw new BadRequestException(ALREADY_EXIST_NICKNAME);
         }
 
-        Volunteer volunteer = volunteerSignUpRequest.toEntity();
+        Volunteer volunteer = request.toEntity();
         volunteer.passwordEncode(passwordEncoder);
         volunteerRepository.save(volunteer);
     }
 
+    public void intermediarySignUp(IntermediarySignUpRequest request, MultipartFile file) {
+        if (intermediaryRepository.existsByEmail(request.email())) {
+            throw new BadRequestException(ALREADY_EXIST_EMAIL);
+        }
+        String authImage = fileService.uploadFile(file, "intermediary");
+        if (authImage == null) {
+            throw new BadRequestException(FILE_NOT_FOUND);
+        }
+        Intermediary intermediary = IntermediarySignUpRequest.toEntity(request, authImage);
+        intermediary.passwordEncode(passwordEncoder);
+        intermediaryRepository.save(intermediary);
+    }
 }
