ee67b92c11970d34239197da217b2734c4309310
hojeong2747 <hojeong2747@gmail.com>
2023-10-31 00:38:38 +0900

    chore: Jwt 인증 필터 NO_CHECK_URLS 변경 (#22)

--- src/main/java/com/pawwithu/connectdog/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/pawwithu/connectdog/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -22,9 +22,8 @@ import java.util.Set;
 @Slf4j
 public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
-    private static final Set<String> NO_CHECK_URLS = new HashSet<>(Arrays.asList("/login")); // "/login"으로 들어오는 요청은 Filter 작동 X
+    private static final Set<String> NO_CHECK_URLS = new HashSet<>(Arrays.asList("volunteers/login", "intermediaries/login")); // 해당 uri로 들어오는 요청은 Filter 작동 X
     private final JwtService jwtService;
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -41,18 +40,6 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
-
-        // Access Token이 만료되어 Refresh Token으로 Access Token 재발급
-//        String refreshToken = jwtService.extractRefreshToken(request).orElse(null);
-//        if (StringUtils.hasText(accessToken) && jwtService.isTokenValid(refreshToken)) {
-//            log.info("AccessToken 재발급");
-//            Long id = jwtService.extractId(refreshToken).orElseThrow(() -> new TokenException(INVALID_TOKEN));
-//            if (isRefreshTokenMatch(id, refreshToken)) {
-//                reIssueRefreshAndAccessToken(response, refreshToken, id);
-//            }
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
 
         log.info("유효한 JWT 토큰이 없습니다. uri: {}, {}", request.getRequestURI(), accessToken);
         filterChain.doFilter(request, response);
