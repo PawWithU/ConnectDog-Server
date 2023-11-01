b156d5912a506a59a917c6c231741403a0837f9b
hojeong2747 <hojeong2747@gmail.com>
2023-11-02 02:04:26 +0900

    chore: Redis 저장 이슈 확인 (#22)

--- src/main/java/com/pawwithu/connectdog/domain/auth/handler/LoginSuccessHandler.java
+++ src/main/java/com/pawwithu/connectdog/domain/auth/handler/LoginSuccessHandler.java
@@ -43,27 +43,26 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
         String roleName = role.substring(6, role.length()-1); // INTERMEDIARY, AUTH_INTERMEDIARY / VOLUNTEER, AUTH_VOLUNTEER
 
         Long id;
-        String accessToken = null;
-        String refreshToken = null;
         if ("INTERMEDIARY".equals(roleName) || "AUTH_INTERMEDIARY".equals(roleName)) {
             id = intermediaryRepository.findByEmail(email)
                     .map(Intermediary::getId)
                     .orElseThrow(() -> new BadRequestException(INTERMEDIARY_NOT_FOUND));
 
-            accessToken = jwtService.createAccessToken(id, "INTERMEDIARY");
-            refreshToken = jwtService.createRefreshToken(id, "INTERMEDIARY");
-            jwtService.updateRefreshToken("INTERMEDIARY", id, refreshToken);
+            roleName = "INTERMEDIARY";
+
         } else if ("VOLUNTEER".equals(roleName) || "AUTH_VOLUNTEER".equals(roleName)) {
             id = volunteerRepository.findByEmail(email)
                     .map(Volunteer::getId)
                     .orElseThrow(() -> new BadRequestException(VOLUNTEER_NOT_FOUND));
 
-            accessToken = jwtService.createAccessToken(id, "VOLUNTEER");
-            refreshToken = jwtService.createRefreshToken(id, "VOLUNTEER");
-            jwtService.updateRefreshToken("VOLUNTEER", id, refreshToken);
+            roleName = "VOLUNTEER";
         } else {
             throw new BadRequestException(INVALID_ROLE_NAME); // 다른 roleName 들어왔을 경우의 예외 처리
         }
+
+        String accessToken = jwtService.createAccessToken(id, roleName);
+        String refreshToken = jwtService.createRefreshToken(id, roleName);
+        jwtService.updateRefreshToken(roleName, id, refreshToken);
 
         // JWT 서비스에서 토큰 정보 가져오기
         Map<String, String> tokenData = jwtService.sendAccessAndRefreshToken(roleName, accessToken, refreshToken);
