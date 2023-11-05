e60ba4e2e1362742da7dde3b67aae4304b4568ee
hojeong2747 <hojeong2747@gmail.com>
2023-11-06 05:31:45 +0900

    refactor: 소셜 로그인 추가 회원가입 요청 Dto Validation 추가 (#29)

--- src/main/java/com/pawwithu/connectdog/domain/auth/dto/request/RefreshTokenRequest.java
+++ src/main/java/com/pawwithu/connectdog/domain/auth/dto/request/RefreshTokenRequest.java
@@ -1,4 +1,7 @@
 package com.pawwithu.connectdog.domain.auth.dto.request;
 
-public record RefreshTokenRequest(String refreshToken) {
+import jakarta.validation.constraints.NotBlank;
+
+public record RefreshTokenRequest(@NotBlank(message = "이메일은 필수 전송 값입니다.")
+                                  String refreshToken) {
 }
