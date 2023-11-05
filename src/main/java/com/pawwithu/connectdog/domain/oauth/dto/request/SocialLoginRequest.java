29d25fc1b5682c85596997d557506fbec0029345
hojeong2747 <hojeong2747@gmail.com>
2023-11-06 06:09:58 +0900

    refactor: 소셜 로그인 요청 Dto SocialType Validation 변경  (#29)

--- src/main/java/com/pawwithu/connectdog/domain/oauth/dto/request/SocialLoginRequest.java
+++ src/main/java/com/pawwithu/connectdog/domain/oauth/dto/request/SocialLoginRequest.java
@@ -2,10 +2,11 @@ package com.pawwithu.connectdog.domain.oauth.dto.request;
 
 import com.pawwithu.connectdog.domain.volunteer.entity.SocialType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record SocialLoginRequest(
         @NotBlank(message = "AccessToken은 필수 전송 값입니다.")
         String accessToken,
-        @NotBlank(message = "provider는 필수 전송 값입니다.")
+        @NotNull(message = "provider는 필수 전송 값입니다.")
         SocialType provider) {
 }
