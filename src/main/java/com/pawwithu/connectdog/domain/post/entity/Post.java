441e57e2cf2d2a67bb171ae701e358bbb135b23c
kyeong-hyeok <minkh4227@naver.com>
2023-11-09 19:58:05 +0900

    feat: Post의 mainImage 필드 PostImage의 외래키로 변경 (#35)

--- src/main/java/com/pawwithu/connectdog/domain/post/entity/Post.java
+++ src/main/java/com/pawwithu/connectdog/domain/post/entity/Post.java
@@ -4,12 +4,9 @@ import com.pawwithu.connectdog.common.entity.BaseTimeEntity;
 import com.pawwithu.connectdog.domain.dog.entity.Dog;
 import com.pawwithu.connectdog.domain.intermediary.entity.Intermediary;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -26,17 +23,18 @@ public class Post extends BaseTimeEntity {
     @Column(length = 20, nullable = false)
     private String arrivalLoc; // 도착 지역
     @Column(nullable = false)
-    private LocalDateTime startDate; // 봉사 시작 가능 날짜
+    private LocalDate startDate; // 봉사 시작 가능 날짜
     @Column(nullable = false)
-    private LocalDateTime endDate; // 봉사 마감 가능 날짜
+    private LocalDate endDate; // 봉사 마감 가능 날짜
     @Column(length = 10)
     private String pickUpTime; // 픽업 시간
     @Column(nullable = false)
     private Boolean isKennel; // 컨넬 제공 여부
     @Column(length = 200, nullable = false)
     private String content; // 이동봉사 설명
-    @Column(nullable = false)
-    private String mainImage; // 대표 이미지
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mainImage_id")
+    private PostImage mainImage; // 대표 이미지
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "intermediary_id", nullable = false)
     private Intermediary intermediary; // 이동봉사 중개 id
@@ -44,4 +42,21 @@ public class Post extends BaseTimeEntity {
     @JoinColumn(name = "dog_id", nullable = false)
     private Dog dog; // 강아지 id
 
+    @Builder
+    public Post(PostStatus status, String departureLoc, String arrivalLoc, LocalDate startDate, LocalDate endDate, String pickUpTime, Boolean isKennel, String content, Intermediary intermediary, Dog dog) {
+        this.status = status;
+        this.departureLoc = departureLoc;
+        this.arrivalLoc = arrivalLoc;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.pickUpTime = pickUpTime;
+        this.isKennel = isKennel;
+        this.content = content;
+        this.intermediary = intermediary;
+        this.dog = dog;
+    }
+
+    public void updateMainImage(PostImage mainImage) {
+        this.mainImage = mainImage;
+    }
 }
