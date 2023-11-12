3ad4273cdb5a04e69076813a595a7ef13d4bc1ad
hojeong2747 <hojeong2747@gmail.com>
2023-11-12 12:45:10 +0900

    refactor: 이동봉사자 봉사 후기 등록 API 요청 Dto @Size validation 추가 (#41)

--- src/main/java/com/pawwithu/connectdog/domain/review/dto/request/ReviewCreateRequest.java
+++ src/main/java/com/pawwithu/connectdog/domain/review/dto/request/ReviewCreateRequest.java
@@ -4,8 +4,10 @@ import com.pawwithu.connectdog.domain.post.entity.Post;
 import com.pawwithu.connectdog.domain.review.entity.Review;
 import com.pawwithu.connectdog.domain.volunteer.entity.Volunteer;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ReviewCreateRequest(@NotBlank(message = "후기 내용은 필수 입력 값입니다.")
+                                  @Size(min=20, max=300, message = "20~300자의 내용이어야 합니다.")
                                   String content) {
 
     public static Review reviewToEntity(ReviewCreateRequest request, Volunteer volunteer, Post post) {
