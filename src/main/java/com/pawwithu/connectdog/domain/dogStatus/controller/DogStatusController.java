30528817ab6fe46f046fd5622ef8cd895bc37546
hojeong2747 <hojeong2747@gmail.com>
2023-11-15 00:35:11 +0900

    chore: 이동봉사자 봉사 후기 등록 API/중개 근황 등록 API 응답 명세 추가 (#63)

--- src/main/java/com/pawwithu/connectdog/domain/dogStatus/controller/DogStatusController.java
+++ src/main/java/com/pawwithu/connectdog/domain/dogStatus/controller/DogStatusController.java
@@ -30,11 +30,11 @@ public class DogStatusController {
 
     private final DogStatusService dogStatusService;
 
-    @Operation(summary = "근황 등록", description = "근황를 등록합니다.",
+    @Operation(summary = "근황 등록", description = "근황을 등록합니다.",
             security = { @SecurityRequirement(name = "bearer-key") },
             responses = {@ApiResponse(responseCode = "204", description = "근황 등록 성공")
                     , @ApiResponse(responseCode = "400"
-                    , description = "V1, 내용은 20~300자로 입력해 주세요. \t\n F1, 파일이 존재하지 않습니다. \t\n F2, 파일 업로드에 실패했습니다. \t\n " +
+                    , description = "V1, 근황 내용은 필수 입력 값입니다. \t\n V1, 내용은 20~300자로 입력해 주세요. \t\n F1, 파일이 존재하지 않습니다. \t\n F2, 파일 업로드에 실패했습니다. \t\n " +
                     "M2, 해당 이동봉사 중개를 찾을 수 없습니다. \t\n P2, 해당 공고를 찾을 수 없습니다."
                     , content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
             })
