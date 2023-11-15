c525a93e62564172390dbe81af573a333517eed1
kyeong-hyeok <minkh4227@naver.com>
2023-11-15 15:33:20 +0900

    chore: 이동봉사 중개 모집중 공고 조회 Dto 필드 순서 수정 (#70)

--- src/main/java/com/pawwithu/connectdog/domain/post/dto/response/PostRecruitingGetResponse.java
+++ src/main/java/com/pawwithu/connectdog/domain/post/dto/response/PostRecruitingGetResponse.java
@@ -5,16 +5,17 @@ import com.pawwithu.connectdog.domain.post.entity.PostStatus;
 
 import java.time.LocalDate;
 
-public record PostRecruitingGetResponse(Long postId, String postStatus, String mainImage, String dogName, String departureLoc, String arrivalLoc,
+public record PostRecruitingGetResponse(Long postId, String postStatus, String mainImage, String dogName,
                                         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-                                     LocalDate startDate,
+                                        LocalDate startDate,
                                         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-                                     LocalDate endDate,
+                                        LocalDate endDate,
+                                        String departureLoc, String arrivalLoc,
                                         String volunteerName) {
 
     // 공고 이동봉사자 이름을 제외한 생성자
-    public PostRecruitingGetResponse(Long postId, PostStatus postStatus, String mainImage, String dogName, String departureLoc, String arrivalLoc,
-                                     LocalDate startDate, LocalDate endDate) {
-        this(postId, postStatus.getKey(), mainImage, dogName, departureLoc, arrivalLoc, startDate, endDate, null);
+    public PostRecruitingGetResponse(Long postId, PostStatus postStatus, String mainImage, String dogName,
+                                     LocalDate startDate, LocalDate endDate, String departureLoc, String arrivalLoc) {
+        this(postId, postStatus.getKey(), mainImage, dogName, startDate, endDate, departureLoc, arrivalLoc, null);
     }
 }
