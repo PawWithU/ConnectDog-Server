527377c1210d040709f205a50c4f7f4e51da81bf
kyeong-hyeok <minkh4227@naver.com>
2023-11-15 14:15:15 +0900

    test: 이동봉사 중개 이동봉사 신청 반려 Controller 테스트 코드 추가 (#68)

--- src/test/java/com/pawwithu/connectdog/domain/application/controller/ApplicationControllerTest.java
+++ src/test/java/com/pawwithu/connectdog/domain/application/controller/ApplicationControllerTest.java
@@ -159,4 +159,19 @@ class ApplicationControllerTest {
         result.andExpect(status().isNoContent());
         verify(applicationService, times(1)).confirmApplication(anyString(), anyLong());
     }
+
+    @Test
+    void 이동봉사_신청_반려() throws Exception {
+        //given
+        Long applicationId = 1L;
+
+        //when
+        ResultActions result = mockMvc.perform(
+                delete("/intermediaries/applications/{applicationId}", applicationId)
+        );
+
+        //then
+        result.andExpect(status().isNoContent());
+        verify(applicationService, times(1)).cancelApplication(anyString(), anyLong());
+    }
 }
