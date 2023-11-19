458c979dafa5556196101433a351db9d3eeb82e6
hojeong2747 <hojeong2747@gmail.com>
2023-11-20 02:27:41 +0900

    feat: 이동봉사 후기 조회 API 프로필 이미지 필드 추가 (#116)

--- src/main/java/com/pawwithu/connectdog/domain/intermediary/dto/response/IntermediaryGetReviewsResponse.java
+++ src/main/java/com/pawwithu/connectdog/domain/intermediary/dto/response/IntermediaryGetReviewsResponse.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.util.List;
 
-public record IntermediaryGetReviewsResponse(String dogName, String volunteerNickname, String mainImage, List<String> images,
+public record IntermediaryGetReviewsResponse(Integer profileImageNum, String dogName, String volunteerNickname, String mainImage, List<String> images,
                                              @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
                                              LocalDate startDate,
                                              @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
@@ -14,15 +14,15 @@ public record IntermediaryGetReviewsResponse(String dogName, String volunteerNic
 ) {
 
     // 후기 이미지 리스트 필드를 제외한 생성자
-    public IntermediaryGetReviewsResponse(String dogName, String volunteerNickname, String mainImage,
+    public IntermediaryGetReviewsResponse(Integer profileImageNum, String dogName, String volunteerNickname, String mainImage,
                                 LocalDate startDate, LocalDate endDate, String departureLoc, String arrivalLoc,
                                 String intermediaryName, String content) {
-        this(dogName, volunteerNickname, mainImage, null, startDate, endDate, departureLoc, arrivalLoc, intermediaryName, content);
+        this(profileImageNum, dogName, volunteerNickname, mainImage, null, startDate, endDate, departureLoc, arrivalLoc, intermediaryName, content);
     }
 
     // 후기 이미지 리스트 필드를 포함한 생성자
     public static IntermediaryGetReviewsResponse of(IntermediaryGetReviewsResponse response, List<String> images) {
-        return new IntermediaryGetReviewsResponse(response.dogName, response.volunteerNickname, response.mainImage, images,
+        return new IntermediaryGetReviewsResponse(response.profileImageNum, response.dogName, response.volunteerNickname, response.mainImage, images,
                 response.startDate, response.endDate, response.departureLoc, response.arrivalLoc, response.intermediaryName, response.content);
     }
 }
