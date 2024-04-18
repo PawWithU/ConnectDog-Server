7a044a20ca206d5ea4284de17b1667e6622a1696
kyeong-hyeok <minkh4227@naver.com>
2024-04-18 21:40:05 +0900

    feat: 공고 강아지 성별, 몸무게 제거 (#159)

--- src/main/java/com/pawwithu/connectdog/domain/dog/entity/Dog.java
+++ src/main/java/com/pawwithu/connectdog/domain/dog/entity/Dog.java
@@ -13,22 +13,17 @@ public class Dog extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(length = 10, nullable = false)
+    @Column(length = 8, nullable = false)
     private String name; // 강아지 이름
     @Column(nullable = false)
     private DogSize size; // 강아지 사이즈
-    @Column(nullable = false)
-    private DogGender gender; // 강아지 성별
-    private Float weight; // 강아지 몸무게
     @Column(length = 200)
     private String specifics; // 강아지 특이사항
 
     @Builder
-    public Dog(String name, DogSize size, DogGender gender, Float weight, String specifics) {
+    public Dog(String name, DogSize size, String specifics) {
         this.name = name;
         this.size = size;
-        this.gender = gender;
-        this.weight = weight;
         this.specifics = specifics;
     }
 }
