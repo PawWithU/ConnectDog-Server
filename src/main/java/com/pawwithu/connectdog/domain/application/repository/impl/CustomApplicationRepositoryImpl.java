4779e3d96067de02654b32d20c9b62597cee6b25
kyeong-hyeok <minkh4227@naver.com>
2024-04-21 22:20:40 +0900

    feat: 이동봉사자 승인 대기중 목록 조회 API 반환값 수정 (#161)

--- src/main/java/com/pawwithu/connectdog/domain/application/repository/impl/CustomApplicationRepositoryImpl.java
+++ src/main/java/com/pawwithu/connectdog/domain/application/repository/impl/CustomApplicationRepositoryImpl.java
@@ -34,13 +34,13 @@ public class CustomApplicationRepositoryImpl implements CustomApplicationReposit
     public List<ApplicationVolunteerWaitingResponse> getVolunteerWaitingApplications(Long volunteerId, Pageable pageable) {
         return queryFactory
                 .select(Projections.constructor(ApplicationVolunteerWaitingResponse.class,
-                        post.id, postImage.image, post.departureLoc, post.arrivalLoc,
+                        post.id, postImage.image, dog.name, post.departureLoc, post.arrivalLoc,
                         post.startDate, post.endDate, post.pickUpTime,
-                        intermediary.name, post.isKennel, application.id))
+                        dog.size, post.isKennel, application.id))
                 .from(application)
                 .join(application.post, post)
-                .join(application.post.intermediary, intermediary)
                 .join(application.post.mainImage, postImage)
+                .join(application.post.dog, dog)
                 .where(application.status.eq(ApplicationStatus.WAITING)
                         .and(application.volunteer.id.eq(volunteerId)))
                 .orderBy(application.createdDate.desc())    // 신청 최신순
