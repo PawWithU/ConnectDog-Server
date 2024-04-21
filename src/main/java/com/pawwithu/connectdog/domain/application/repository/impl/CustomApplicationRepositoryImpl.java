2094b58fb52129713b0af4364208e2bc3e147024
kyeong-hyeok <minkh4227@naver.com>
2024-04-21 22:36:22 +0900

    feat: 이동봉사자 봉사 완료 목록 조회 API 반환값 수정 (#161)

--- src/main/java/com/pawwithu/connectdog/domain/application/repository/impl/CustomApplicationRepositoryImpl.java
+++ src/main/java/com/pawwithu/connectdog/domain/application/repository/impl/CustomApplicationRepositoryImpl.java
@@ -133,13 +133,13 @@ public class CustomApplicationRepositoryImpl implements CustomApplicationReposit
     public List<ApplicationVolunteerCompletedResponse> getVolunteerCompletedApplications(Long volunteerId, Pageable pageable) {
         return queryFactory
                 .select(Projections.constructor(ApplicationVolunteerCompletedResponse.class,
-                        post.id, postImage.image, post.departureLoc, post.arrivalLoc,
+                        post.id, postImage.image, dog.name, post.departureLoc, post.arrivalLoc,
                         post.startDate, post.endDate, post.pickUpTime,
-                        intermediary.name, post.isKennel, review.id))
+                        dog.size, post.isKennel, review.id))
                 .from(application)
                 .join(application.post, post)
-                .join(application.post.intermediary, intermediary)
                 .join(application.post.mainImage, postImage)
+                .join(application.post.dog, dog)
                 .leftJoin(review).on(post.id.eq(review.post.id))
                 .where(application.status.eq(ApplicationStatus.COMPLETED)
                         .and(application.volunteer.id.eq(volunteerId)))
