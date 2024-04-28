16db9dcc10f2964872457e05e0e06d91394349dc
kyeong-hyeok <minkh4227@naver.com>
2024-04-28 16:04:07 +0900

    feat: 이동봉사 모집자 모집중, 모집 마감 목록 조회 API 반환값 수정 (#163)

--- src/main/java/com/pawwithu/connectdog/domain/post/dto/response/PostRecruitingGetResponse.java
+++ src/main/java/com/pawwithu/connectdog/domain/post/dto/response/PostRecruitingGetResponse.java
@@ -1,21 +1,26 @@
 package com.pawwithu.connectdog.domain.post.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.pawwithu.connectdog.domain.dog.entity.DogSize;
 import com.pawwithu.connectdog.domain.post.entity.PostStatus;
 
 import java.time.LocalDate;
 
 public record PostRecruitingGetResponse(Long postId, String postStatus, String mainImage, String dogName,
+                                        String departureLoc, String arrivalLoc,
                                         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
                                         LocalDate startDate,
                                         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
                                         LocalDate endDate,
-                                        String departureLoc, String arrivalLoc,
-                                        String volunteerName) {
+                                        String pickUpTime,
+                                        String dogSize,
+                                        Boolean isKennel) {
 
     // 공고 이동봉사자 이름을 제외한 생성자
     public PostRecruitingGetResponse(Long postId, PostStatus postStatus, String mainImage, String dogName,
-                                     LocalDate startDate, LocalDate endDate, String departureLoc, String arrivalLoc) {
-        this(postId, postStatus.getKey(), mainImage, dogName, startDate, endDate, departureLoc, arrivalLoc, null);
+                                     String departureLoc, String arrivalLoc, LocalDate startDate, LocalDate endDate,
+                                     String pickUpTime, DogSize dogSize, Boolean isKennel) {
+        this(postId, postStatus.getKey(), mainImage, dogName, departureLoc, arrivalLoc,
+                startDate, endDate, pickUpTime, dogSize.getKey(), isKennel);
     }
 }
