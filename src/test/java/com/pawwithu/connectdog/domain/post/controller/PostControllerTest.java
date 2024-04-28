15a7bf7007c99fe4a64c19b475b97894dd5db7c6
kyeong-hyeok <minkh4227@naver.com>
2024-04-28 16:04:51 +0900

    test: 이동봉사 모집자 모집중, 모집 마감 목록 조회 API 테스트 코드 수정 (#163)

--- src/test/java/com/pawwithu/connectdog/domain/post/controller/PostControllerTest.java
+++ src/test/java/com/pawwithu/connectdog/domain/post/controller/PostControllerTest.java
@@ -174,10 +174,10 @@ class PostControllerTest {
         List<PostRecruitingGetResponse> response = new ArrayList<>();
         LocalDate startDate = LocalDate.of(2023, 10, 2);
         LocalDate endDate = LocalDate.of(2023, 11, 7);
-        response.add(new PostRecruitingGetResponse(1L, PostStatus.RECRUITING, "image1", "포포1", startDate, endDate,
-                "서울시 성북구", "서울시 중랑구"));
-        response.add(new PostRecruitingGetResponse(2L, PostStatus.EXPIRED, "image2", "포포2", startDate, endDate,
-                "서울시 성북구", "서울시 중랑구"));
+        response.add(new PostRecruitingGetResponse(1L, PostStatus.RECRUITING.getKey(), "image1", "잔디", "서울시 성북구", "서울시 중랑구",
+                startDate, endDate, "13:00", DogSize.MEDIUM.getKey(), true));
+        response.add(new PostRecruitingGetResponse(2L, PostStatus.RECRUITING.getKey(), "image1", "잔디", "서울시 성북구", "서울시 중랑구",
+                startDate, endDate, "13:00", DogSize.MEDIUM.getKey(), true));
 
 
         //when
