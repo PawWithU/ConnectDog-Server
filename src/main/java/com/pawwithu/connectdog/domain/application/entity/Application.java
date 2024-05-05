a04460752fbf909f1ef9d4d67b0845188934e26a
kyeong-hyeok <minkh4227@naver.com>
2024-05-05 17:20:32 +0900

    feat: 이동봉사 신청 DB 제약 조건 해제 및 API 수정 (#177)

--- src/main/java/com/pawwithu/connectdog/domain/application/entity/Application.java
+++ src/main/java/com/pawwithu/connectdog/domain/application/entity/Application.java
@@ -11,7 +11,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"post_id"}))
+//@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"post_id"}))
 public class Application extends BaseTimeEntity {
 
     @Id
