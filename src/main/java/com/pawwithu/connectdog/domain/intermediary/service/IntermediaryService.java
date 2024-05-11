3cfea52f5360d01e3bbd9f9d97dcfb1ab5f43057
hojeong2747 <hojeong2747@gmail.com>
2024-05-11 11:21:14 +0900

    feat: 이동봉사자 모집자 프로필 - 기본 정보 조회 API 반환값 수정 (#187)

--- src/main/java/com/pawwithu/connectdog/domain/intermediary/service/IntermediaryService.java
+++ src/main/java/com/pawwithu/connectdog/domain/intermediary/service/IntermediaryService.java
@@ -50,16 +50,7 @@ public class IntermediaryService {
     @Transactional(readOnly = true)
     public IntermediaryGetInfoResponse getIntermediaryInfo(Long intermediaryId) {
         Intermediary intermediary = intermediaryRepository.findById(intermediaryId).orElseThrow(() -> new BadRequestException(INTERMEDIARY_NOT_FOUND));
-        // 봉사 완료 건수
-        Long completedPostCount = customPostRepository.getCountOfCompletedPosts(intermediaryId);
-
-        // 받은 후기 총 건수
-        Long reviewCount = customReviewRepository.getIntermediaryCountOfReviews(intermediaryId);
-
-        // 남긴 근황 총 건수
-        Long dogStatusCount = customDogStatusRepository.getIntermediaryCountOfDogStatuses(intermediaryId);
-
-        IntermediaryGetInfoResponse intermediaryInfo = IntermediaryGetInfoResponse.from(intermediary, completedPostCount, reviewCount, dogStatusCount);
+        IntermediaryGetInfoResponse intermediaryInfo = IntermediaryGetInfoResponse.from(intermediary);
         return intermediaryInfo;
     }
 
