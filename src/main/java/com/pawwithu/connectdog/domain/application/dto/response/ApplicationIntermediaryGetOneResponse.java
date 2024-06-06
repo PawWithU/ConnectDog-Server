f1cd25c71feabb3cdda82e43f865991aae3cdddf
kyeong-hyeok <minkh4227@naver.com>
2024-06-06 23:13:39 +0900

    feat: 모집자 신청 내역 단건 조회 API 수정 (#240)

--- src/main/java/com/pawwithu/connectdog/domain/application/dto/response/ApplicationIntermediaryGetOneResponse.java
+++ src/main/java/com/pawwithu/connectdog/domain/application/dto/response/ApplicationIntermediaryGetOneResponse.java
@@ -8,10 +8,11 @@ import java.time.LocalDate;
 public record ApplicationIntermediaryGetOneResponse(Long id,
                                                     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
                                                     LocalDate createdDate,
+                                                    String nickname,
                                                     String volunteerName, String phone, String content) {
 
     public static ApplicationIntermediaryGetOneResponse from(Application application) {
-        return new ApplicationIntermediaryGetOneResponse(application.getId(), application.getCreatedDate().toLocalDate(),
+        return new ApplicationIntermediaryGetOneResponse(application.getId(), application.getCreatedDate().toLocalDate(), application.getVolunteer().getNickname(),
                 application.getVolunteerName(), application.getPhone(), application.getContent());
     }
 
